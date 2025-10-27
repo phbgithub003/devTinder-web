eb617529e1ebba57951fd81ec21c8907b7197d91
Harshit Bhargava <palacheralaharshit@gmail.com>
2025-10-27 22:53:18 +0530

    page navigation and user login

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,13 +1,42 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import axios from "axios";
+import { API_BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useEffect } from "react";
 
 function Body() {
+  const dispatch = useDispatch();
+  const Navigate = useNavigate();
+  const userData = useSelector((store) => store.user);
+
+  const fetchUser = async () => {
+    if (userData) return; // If user data already exists, no need to fetch again
+    try {
+      const res = await axios.get(API_BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+      dispatch(addUser(res.data));
+    } catch (err) {
+      if (err.response && err.response.status === 401) {
+        Navigate("/login");
+        return;
+      }
+      console.error("Error fetching user data:", err);
+    }
+  };
+
+  useEffect(() => {
+      fetchUser();
+  }, []);
+
   return (
     <>
-        <Navbar/>
-        <Outlet/>
-        <Footer/>
+      <Navbar />
+      <Outlet />
+      <Footer />
     </>
   );
 }
